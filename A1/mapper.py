b514876d24c059cbbd2e6d39339290cda82b1c32
yangahmed <jiayi@DESKTOP-6ICPTPC.localdomain>
2019-04-26 01:35:13 +0200

    update

--- A1/mapper.py
+++ A1/mapper.py
@@ -3,38 +3,41 @@
 
 import sys
 import json
+import re
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     # parse the tweets
-    tweet = json.loads(line)
+    try:
+        tweet = json.loads(line)
+    except:
+        continue
     # disregard the "retweets"
     if 'retweeted_status' not in tweet:
         # read the text
         text = tweet['text']
-        # split the line into words
-        words = text.split()
+
         # increase counters
-        for word in words:
-            # write the results to STDOUT (standard output);
-            # what we output here will be the input for the
-            # Reduce step, i.e. the input for reducer.py
-            #
-            # tab-delimited; the trivial word count is 1
-            if word.lower() == 'han':
-                print(word, 1, sep='\t') 
-            elif word.lower() == 'hon':
-                print(word, 1, sep='\t') 
-            elif word.lower() == 'den':
-                print(word, 1, sep='\t') 
-            elif word.lower() == 'det':
-                print(word, 1, sep='\t') 
-            elif word.lower() == 'denna':
-                print(word, 1, sep='\t')
-            elif word.lower() == 'denne':
-                print(word, 1, sep='\t') 
-            elif word.lower() == 'hen':
-                print(word, 1, sep='\t')
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        if re.search('han', text, re.I):
+            print('han', 1, sep='\t')
+        if re.search('hon', text, re.I):
+            print('hon', 1, sep='\t')
+        if re.search('den', text, re.I):
+            print('den', 1, sep='\t')
+        if re.search('det', text, re.I):
+            print('det', 1, sep='\t')
+        if re.search('denna', text, re.I):
+            print('denna', 1, sep='\t')
+        if re.search('denne', text, re.I):
+            print('denne', 1, sep='\t')
+        if re.search('hen', text, re.I):
+            print('hen', 1, sep='\t')
+        
         
         # count this tweet as 'unique'
         print('total_unique', 1, sep='\t') 
